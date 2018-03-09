655b5fbc1a05c70c2032163063f6a7b08c242f8f
Alexey <my@.com>
2018-03-09 18:25:57 +0300

    addin post info pages

--- newBlogg/urls.py
+++ newBlogg/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
+    url(r'^post/(?P<pk>\d+)/$', views.post_info, name='post_info'),
 ]
--- newBlogg/views.py
+++ newBlogg/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+
 from .models import PostConstructor
+from django.shortcuts import get_object_or_404, render
 
 
 def post_list(request):
     posts = PostConstructor.objects.all()
-    return render(request, 'newBlogg/index.html', {'posts': posts})
+    return render(request, 'newBlogg/list.html', {'posts': posts})
+
+
+def post_info(request, pk):
+    PostConstructor.objects.get(pk=pk)
+    post = get_object_or_404(PostConstructor, pk=pk)
+    return render(request, 'newBlogg/post_info.html', {'post': post})
