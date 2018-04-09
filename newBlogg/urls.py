f89645571e323a0e25c88731f005b12bf650e1c4
Alexey <my@.com>
2018-04-09 15:23:46 +0300

    adding template for registration

--- newBlogg/urls.py
+++ newBlogg/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.sign_in, name='sign_in'),
     url(r'^post/(?P<pk>\d+)/$', views.post_info, name='post_info'),
+    url(r'^post/(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
     url(r'^post/new/$', views.post_new, name='post_new'),
+    url(r'^posts/$', views.post_list, name='post_list'),
+
 ]
--- newBlogg/views.py
+++ newBlogg/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth import authenticate, login
 
 from .models import PostConstructor
 from django.shortcuts import get_object_or_404, render
 from .forms import PostForm
 from django.utils import timezone
 from django.shortcuts import redirect
+from django.contrib.auth.forms import UserCreationForm
 
 
 def post_list(request):
@@ -31,9 +33,31 @@ def post_new(request):
     return render(request, 'newBlogg/post_edit.html', {'form': form})
 
 
+def post_edit(request, pk):
+    post = get_object_or_404(PostConstructor, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.pubDate = timezone.now()
+            post.save()
+            return redirect('post_info', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'newBlogg/post_edit.html', {'form': form})
 
 
-
-
-
-
+def sign_in(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            my_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=my_password)
+            login(request, user)
+            return redirect('post_list')
+    else:
+        form = UserCreationForm()
+    return render(request, 'newBlogg/registration_template.html', {'form': form})
